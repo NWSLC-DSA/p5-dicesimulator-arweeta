6f3983ebff91cbcf783a819c99d33d0ebebcc572
arweeta <94476382+arweeta@users.noreply.github.com>
2022-05-05 12:00:25 +0100

    Added button & if statement
    added button and if statement

--- sketch.js
+++ sketch.js
@@ -1,12 +1,54 @@
-function setup() {
+//values for dice rolls
+const diceValue = [diceValueOne, diceValueTwo, diceValueThree, diceValueFour, diceValueFive, diceValueSix]
+let diceRoll = 0;
+
+//values for button
+let button;
+let buttonHasBeenPressed;
+ 
+ function setup() {
   createCanvas(400, 400);
+
+  button = createButton("ٌRoll the dice");
+  button.position(150, 150);
+  button.mousePressed(buttonPressed);
+  
+}
+
+//function for clicking the button
+function buttonPressed(){
+  buttonHasBeenPressed = true;
+  diceRoll = floor(random(1,7));
+  console.log (diceRoll);
 }
 
 function draw() {
   background(220);
-  diceValueOne(40, 20, 20);
+  
+    if (buttonHasBeenPressed) {
+    //console.log ("mousePress")
+    if (diceRoll == 1){
+    diceValue [0] (80, 155, 60);}
+   else if 
+     (diceRoll == 2){
+    diceValue [1](80, 155, 60);}
+  else if 
+      (diceRoll == 3){
+    diceValue [2](80, 155, 60);}
+  else if 
+      (diceRoll == 4){
+    diceValue [3](80, 155, 60);}
+  else if 
+      (diceRoll == 5){
+    diceValue [4](80, 155, 60);}
+    else if 
+      (diceRoll == 6){
+    diceValue [5](80, 155, 60);}
+    }
 
-}
+  }
+
+
 
 //This is a user defined function for drawing a dice with the value one.
 function diceValueOne(size, xPoint, yPoint){
